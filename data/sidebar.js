06ddbf8446fda99a626c209365dd6d86d891650b
PATRICK <kernp@gmx.de>
2015-11-08 13:32:58 +0100

    update sidebar.js

--- data/sidebar.js
+++ data/sidebar.js
@@ -2,17 +2,17 @@
 let windowsList = document.getElementById("windows");
 let tabTemplate = document.getElementById("tab").content;
 
-addon.port.on("everything", function(windows) {
-	tabTemplate.querySelector("span.mutelabel").textContent =
-			" " + document.getElementById("mutelabel").textContent;
+tabTemplate.querySelector("span.mutelabel").textContent =
+  " " + document.querySelector('[data-l10n-id="mute.label"]').textContent;
 
+addon.port.on("everything", function(windows) {
 	windowsList.innerHTML = "";
 	for (let tabs of windows) {
 		let windowItem = document.createElement("li");
 		let tabsList = document.createElement("ul");
 		for (let tab of tabs) {
-			let listItem = tabTemplate.cloneNode(true).firstElementChild;
-			listItem.setAttribute("id", tab.id);
+      let listItem = tabTemplate.cloneNode(true).firstElementChild;
+      listItem.setAttribute("id", tab.id);
 			listItem.querySelector("input[type=\"checkbox\"]").onclick = onMuteClick;
 			updateTab(listItem, tab);
 			tabsList.appendChild(listItem);
@@ -20,11 +20,6 @@ addon.port.on("everything", function(windows) {
 		windowItem.appendChild(tabsList);
 		windowsList.appendChild(windowItem);
 	}
-});
-
-addon.port.on("tabchanged", function(tab) {
-	let listItem = document.getElementById(tab.id);
-	updateTab(listItem, tab);
 });
 
 function getListItem(element) {
@@ -36,16 +31,14 @@ function getListItem(element) {
 }
 
 function onMuteClick() {
-	let listItem = getListItem(this);
-
 	addon.port.emit("audioStateChanged", {
-		id: listItem.id,
-		state: this.checked
+		id: getListItem(this).id
 	});
 }
 
 function updateTab(listItem, tab) {
 	listItem.querySelector(".tabicon > img").src = tab.icon ? tab.icon : "chrome://mozapps/skin/places/defaultFavicon.png";
 	listItem.querySelector(".tabtitle").textContent = tab.title;
-	listItem.querySelector("input[type=\"checkbox\"]").checked = tab.noisy.indexOf("muted") >= 0;
+	listItem.querySelector(".tabtitle").setAttribute("title", tab.title);
+	listItem.querySelector("input[type=\"checkbox\"]").checked = tab.muted == "true";
 }
